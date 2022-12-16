3a78844fa3d026892c04bd00f919be5b3ff8b4cb
PatMAllen <116239708+PatMAllen@users.noreply.github.com>
2022-12-16 01:20:16 -0500

    Updates

---
+++ Assets/scripts.js
@@ -0,0 +1,29 @@
+// 1. You will have only <span>20 seconds</span> per each question.
+// 2. Once you select your answer, you can't reselect.
+// 3. You can't select any option once time goes off.
+// 4. You can't exit from the Quiz while you're playing.
+// 5. You'll get points on the basis of your correct answers.
+
+// What categorey does apples fall under ?
+// Fruits
+// Carbohydrates
+// Liquids
+// Protiens
+
+// What categorey does an Chicken fall under ?
+// Protien
+// Carbohydrates
+// Grains
+// Oils
+
+// What categorey does Vegetable Oil fall under ?
+// Protiens
+// Oils
+// Grains
+// Fruits
+
+// What categorey does Rice fall under ?
+// Oils
+// Vegetables
+// Protiens
+// Grains
